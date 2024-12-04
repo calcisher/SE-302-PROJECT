f02fc1297519536e76e79bf8f72c65e6e990989c
Ediz Arkın Kobak <119970256+EdizArkin@users.noreply.github.com>
2024-12-04 22:11:36 +0300

    MainViewSetupTry

--- src/main/java/ClassAssignmentSystem/MainViewController.java
+++ src/main/java/ClassAssignmentSystem/MainViewController.java
@@ -1,23 +1,25 @@
 package ClassAssignmentSystem;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 
-import java.io.IOException;
-
-public class MainViewController extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+public class MainViewController {
+    @FXML
+    private void onManageCourse(ActionEvent event) {
+        showAlert("Manage Course Clicked!");
     }
 
-    public static void main(String[] args) {
-        launch();
+    @FXML
+    private void onCreateMeeting(ActionEvent event) {
+        showAlert("Create Meeting Clicked!");
+    }
+
+    private void showAlert(String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Action");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
     }
 }
